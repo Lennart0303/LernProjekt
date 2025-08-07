42399ea9a0fe5cb353d68091bed43d1ea26289fd
Lennart Vermehr <Lennart.V@t-online.de>
2025-08-07 03:58:04 +0200

    Rate Limit eingeführt Mit Caffine Cache für die Tokens sowie CorsHeader Hinzugefügt auhc bei Bucke4j Respones

--- myapp/backend/src/main/java/Model/BackendApplication.java
+++ myapp/backend/src/main/java/Model/BackendApplication.java
@@ -2,12 +2,13 @@ package Model;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching // nötig für RateLimit und Tokens zu Cachen
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(BackendApplication.class, args);
+  }
 }
--- myapp/backend/src/main/java/Model/Security/SecurityConfig.java
+++ myapp/backend/src/main/java/Model/Security/SecurityConfig.java
@@ -1,8 +1,11 @@
 package Model.Security;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -25,83 +28,116 @@ import org.springframework.security.config.annotation.authentication.configurati
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    private final JwtUtil jwtUtil;
+        private final JwtUtil jwtUtil;
 
-    public SecurityConfig(JwtUtil jwtUtil) {
-        this.jwtUtil = jwtUtil;
-    }
+        public SecurityConfig(JwtUtil jwtUtil) {
+                this.jwtUtil = jwtUtil;
+        }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                // 1) HTTP → HTTPS umleiten
-                .redirectToHttps(withDefaults())
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                http
+                                // 1) HTTP → HTTPS umleiten
+                                .redirectToHttps(withDefaults())
 
-                // 2) HSTS aktivieren
-                .headers(headers -> headers.httpStrictTransportSecurity(hsts -> hsts.includeSubDomains(true)
-                        .maxAgeInSeconds(31_536_000))
-                        .contentSecurityPolicy(csp -> csp
-                                .policyDirectives(
-                                        "default-src 'self'; " +
-                                                "script-src 'self'; " + // ← nur eigene .js-Dateien
-                                                "style-src 'self' 'unsafe-inline'; " +
-                                                "img-src 'self' data:; " +
-                                                "connect-src 'self' https://localhost:8443; " +
-                                                "font-src 'self'; " +
-                                                "frame-ancestors 'none';")))
+                                // 2) HSTS aktivieren
+                                .headers(headers -> headers
+                                                .httpStrictTransportSecurity(hsts -> hsts.includeSubDomains(true)
+                                                                .maxAgeInSeconds(31_536_000))
+                                                .contentSecurityPolicy(csp -> csp
+                                                                .policyDirectives(
+                                                                                "default-src 'self'; " +
+                                                                                                "script-src 'self'; " + // ←
+                                                                                                                        // nur
+                                                                                                                        // eigene
+                                                                                                                        // .js-Dateien
+                                                                                                "style-src 'self' 'unsafe-inline'; "
+                                                                                                +
+                                                                                                "img-src 'self' data:; "
+                                                                                                +
+                                                                                                "connect-src 'self' https://localhost:8443; "
+                                                                                                +
+                                                                                                "font-src 'self'; " +
+                                                                                                "frame-ancestors 'none';")))
 
-                .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .sessionManagement(sm -> sm
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        // öffentlich: Login/Register
-                        .requestMatchers("/api/auth/**").permitAll()
-                        // nur Admins: z.B. Nutzerverwaltung
-                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
-                        // normale User‑Funktionen
-                        .requestMatchers("/api/meal/**").hasAnyRole("USER", "ADMIN")
-                        // feedback
-                        .requestMatchers("/api/feedback/**").hasAnyRole("USER", "ADMIN")
-                        // alles andere: angemeldet
-                        .anyRequest().authenticated())
-                .addFilterBefore(new JwtFilter(jwtUtil),
-                        UsernamePasswordAuthenticationFilter.class);
+                                .csrf(csrf -> csrf.disable())
+                                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                                .sessionManagement(sm -> sm
+                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                .authorizeHttpRequests(auth -> auth
+                                                // öffentlich: Login/Register
+                                                .requestMatchers("/api/auth/**").permitAll()
+                                                // nur Admins: z.B. Nutzerverwaltung
+                                                .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                                                // normale User‑Funktionen
+                                                .requestMatchers("/api/meal/**").hasAnyRole("USER", "ADMIN")
+                                                // feedback
+                                                .requestMatchers("/api/feedback/**").hasAnyRole("USER", "ADMIN")
+                                                // alles andere: angemeldet
+                                                .anyRequest().authenticated())
+                                .addFilterBefore(new JwtFilter(jwtUtil),
+                                                UsernamePasswordAuthenticationFilter.class);
 
-        return http.build();
-    }
+                return http.build();
+        }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
 
-    @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration cfg) throws Exception {
-        return cfg.getAuthenticationManager();
-    }
+        @Bean
+        public AuthenticationManager authenticationManager(
+                        AuthenticationConfiguration cfg) throws Exception {
+                return cfg.getAuthenticationManager();
+        }
 
-    @Bean
-    public UserDetailsService userDetailsService(UserRespository repo) {
-        return username -> repo.findByUsername(username)
-                .map(u -> User.builder() // Spring-UserDetails
-                        .username(u.getUsername())
-                        .password(u.getPasswordHash()) // muss mit demEncoder übereinstimmen
-                        .roles(u.getRole()) // z.B. "USER" oder "ADMIN"
-                        .build())
-                .orElseThrow(() -> new UsernameNotFoundException("User nicht gefunden"));
-    }
+        @Bean
+        public UserDetailsService userDetailsService(UserRespository repo) {
+                return username -> repo.findByUsername(username)
+                                .map(u -> User.builder() // Spring-UserDetails
+                                                .username(u.getUsername())
+                                                .password(u.getPasswordHash()) // muss mit demEncoder übereinstimmen
+                                                .roles(u.getRole()) // z.B. "USER" oder "ADMIN"
+                                                .build())
+                                .orElseThrow(() -> new UsernameNotFoundException("User nicht gefunden"));
+        }
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration cfg = new CorsConfiguration();
-        cfg.setAllowedOrigins(List.of("https://localhost:3000")); // dein React‑Host
-        cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        cfg.setAllowedHeaders(List.of("*"));
-        cfg.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource src = new UrlBasedCorsConfigurationSource();
-        src.registerCorsConfiguration("/**", cfg);
-        return src;
-    }
+        @Bean
+        public CorsConfigurationSource corsConfigurationSource() {
+                CorsConfiguration cfg = new CorsConfiguration();
+                cfg.setAllowedOrigins(List.of("https://localhost:3000")); // dein React‑Host
+                cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                cfg.setAllowedHeaders(List.of("*"));
+                cfg.setAllowCredentials(true);
+                UrlBasedCorsConfigurationSource src = new UrlBasedCorsConfigurationSource();
+                src.registerCorsConfiguration("/**", cfg);
+                return src;
+        }
+
+        // Bei Rate Limit müssen trotzdem Cors angehängt werden, damit der Browser der die
+        // Anfrage annimmt ansonsten schickt das Bucket4j ohne CORS-Header
+        @Bean
+        public FilterRegistrationBean<CorsFilter> corsFilterRegistration() {
+                // 1) Definiere CORS-Regeln
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(List.of("https://localhost:3000"));
+                config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                config.setAllowedHeaders(List.of("*"));
+                config.setAllowCredentials(true);
+                config.setExposedHeaders(List.of("Retry-After", "X-Rate-Limit-Remaining"));
+
+                // 2) URL-Pfade, auf die CORS angewendet wird
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", config);
+
+                // 3) Erzeuge den Filter
+                CorsFilter corsFilter = new CorsFilter(source);
+
+                // 4) Registriere ihn mit höchster Priorität
+                FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(corsFilter);
+                bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+                bean.addUrlPatterns("/*");
+                return bean;
+        }
 }
